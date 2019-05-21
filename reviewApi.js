ab27aeecde1692f8ec3703160a699119d0aa68a8
Malyellen Vang <50208740+MalyElle@users.noreply.github.com>
2019-05-21 11:58:47 -0500

    Commiting Update on API/JS

--- reviewApi.js
+++ reviewApi.js
@@ -1,5 +1,7 @@
 var ironmanDiv = document.getElementById('ironMan');
 
+var backgroundColor = document.body.style.backgroundColor = "lightgray";
+
 var container = document.createElement('div');
 
 var logo = document.createElement('img');
@@ -7,8 +9,10 @@ logo.src = 'shutterstock.jpg';
 logo.style.display = 'block'; 
 logo.style.marginLeft = 'auto';
 logo.style.marginRight = 'auto';
-logo.style.height = '500px';
-logo.style.width = '700px';
+logo.style.height = '400px';
+logo.style.width = '1300px';
+logo.classList.add('w3-black','w3-border','w3-margin')
+
 
 ironmanDiv.appendChild(container);
 container.appendChild(logo);
@@ -22,12 +26,35 @@ request.onload = function() {
         data.forEach(info => {
             var card = document.createElement('div');
             var heading1 = document.createElement('h1');
-            heading1.textContent = info.company;
+            heading1.textContent = info.name;
+            heading1.style.color = 'black'
+            heading1.classList.add('w3-teal','w3-center','w3-margin',)
+            
+            var para = document.createElement('li');
+            info.name = info.name.substring(0,100);
+            para.textContent = `${info.username}`;
+            para.style.listStyle = 'square inside url("purple")';
+            para.style.fontSize = '1.3rem'
 
+           var website = document.createElement('li');
+           website.textContent = info.website;
+           website.style.listStyle = 'square inside url("sqpurple.gif")';
+           website.style.fontSize = '1.3rem'
 
+           
+
+           var mail = document.createElement('li');
+           info.email = info.email.substring(0,100);
+           mail.textContent =`${info.email}`;
+           mail.style.listStyle = 'square inside url("sqpurple.gif")';
+           mail.style.fontSize = '1.3rem'
 
 
             container.appendChild(card);
+            card.appendChild(heading1);
+            card.appendChild(para);
+            card.appendChild(website);
+            card.appendChild(mail);
             
         });
     } else {
