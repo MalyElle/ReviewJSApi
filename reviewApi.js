075f9badced21ce8373380b8f3e72a73a952b616
Malyellen Vang <50208740+MalyElle@users.noreply.github.com>
2019-05-21 08:46:12 -0500

    Update on Reviewing API

---
+++ reviewApi.js
@@ -0,0 +1,31 @@
+var ironmanDiv = document.getElementById('ironMan');
+
+var container = document.createElement('div');
+
+var logo = document.createElement('img');
+logo.src = 'shutterstock.jpg';
+
+ironmanDiv.appendChild(container);
+container.appendChild(logo);
+
+var request = new XMLHttpRequest();
+request.open('GET', 'https://jsonplaceholder.typicode.com/users', true);
+
+request.onload = function() {
+    var data = JSON.parse(this.response);
+    if(request.status >= 200 && request.status < 400) {
+        data.forEach(info => {
+            var card = document.createElement('div');
+            var heading1 = document.createElement('h1');
+            heading1.textContent = info.company;
+
+
+
+
+            container.appendChild(card);
+            
+        });
+    } else {
+        alert('VIRUS!')
+    }
+}
